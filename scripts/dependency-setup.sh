00040d977410f9a59b5fb10f390edb5611cdf2bb
squ27 <36833926+squ27@users.noreply.github.com>
2019-11-23 19:26:23 -0600

    Create dependency-setup.sh

---
+++ scripts/dependency-setup.sh
@@ -0,0 +1,11 @@
+sudo apt-get update -y
+sudo apt-get install software-properties-common
+sudo apt-get upgrade -y
+sudo apt-get install git build-essential automake cmake libtool zip libunwind-setjmp0-dev zlib1g-dev unzip pkg-config -y
+sudo apt-get install -y tar wget git
+sudo apt-get install -y autoconf libtool
+sudo apt-get -y install build-essential python-dev libcurl4-nss-dev libsasl2-dev libsasl2-modules maven libapr1-dev libsvn-dev
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update -y
+sudo apt-get install oracle-java8-installer -y
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
